d5d0eebf04774a8e2d8176459809bb35ec6c5b77
tbshiki <info@tbshiki.com>
2022-07-02 14:55:51 +0900

    Update シート追加・シートコピーの時に同名のものがあれば連番をつけて実行する関数を追加

--- xlwings_operation/xlwings_me.py
+++ xlwings_operation/xlwings_me.py
@@ -1,3 +1,4 @@
+from itsdangerous import want_bytes
 import xlwings as xw
 from importlib import import_module
 
@@ -73,10 +74,133 @@ import win32con
 
 
 def MessageBox(application, alert="エラーが発生しました", title="エラー", button="MB_OK", icon="MB_ICONERROR"):
-    flg = win32api.MessageBox(
-        application.app.hwnd,
-        alert,
-        title,
-        win32con.__dict__[button] | win32con.__dict__[icon],
-    )
+    flg = win32api.MessageBox(application.app.hwnd, alert, title, win32con.__dict__[button] | win32con.__dict__[icon],)
     return flg
+
+
+def FreezePanes(ws=None, wb=None, row=1, col=0):
+    """ウインドウ枠の固定
+
+    Args:
+        ws (_type_, optional): _description_. Defaults to xw.books.active.app.api.ActiveWindow.
+        wb (_type_, optional): _description_. Defaults to xw.books.active.
+        row (int, optional): _description_. Defaults to 1.
+        col (int, optional): _description_. Defaults to 0.
+    """
+
+    if ws == None:
+        try:
+            ws = xw.books.active.sheets.active
+        except:
+            return False
+
+    ws.activate()
+    wb = xw.books.active
+    aw = wb.app.api.ActiveWindow
+    aw.FreezePanes = False
+    aw.SplitColumn = col
+    aw.SplitRow = row
+    aw.FreezePanes = True
+
+
+def FreezePanes0(ws=None, wb=None, row=0, col=0):
+    """ウインドウ枠固定の解除
+
+    Args:
+        ws (_type_, optional): _description_. Defaults to xw.books.active.app.api.ActiveWindow.
+        wb (_type_, optional): _description_. Defaults to xw.books.active.
+        row (int, optional): _description_. Defaults to 1.
+        col (int, optional): _description_. Defaults to 0.
+    """
+    if ws == None:
+        try:
+            ws = xw.books.active.sheets.active
+        except:
+            return False
+
+    ws.activate()
+    wb = xw.books.active
+    aw = wb.app.api.ActiveWindow
+    aw.FreezePanes = False
+    aw.SplitColumn = 0
+    aw.SplitRow = 0
+
+
+def check_sheet_add(sheet_name, wb=None, position=0):
+    """同名シートが存在するかチェックしてシート追加
+
+    Args:
+        sheet_name (str): シート名
+        wb (xw.Book, optional): xw.Book. Defaults to None.
+        position (int, optional): 位置. Defaults to 0.
+
+    Returns:
+        bool: True:存在する, False:存在しない
+        sh: シート
+    """
+
+    if wb == None:  # Excelが起動していない場合はFalseを返す
+        try:
+            wb = xw.books.active
+        except:
+            return False
+
+    try:
+        sh = wb.sheets.add(sheet_name, before=wb.sheets[position])
+    except:
+        sh = wb.sheets[sheet_name]
+        all_sh_name = [sh.name for sh in wb.sheets]
+        counter = 2
+
+        while True:
+            if f"{sheet_name} ({counter})" in all_sh_name:
+                counter += 1
+                if counter > 50:
+                    return False  # 50も作成してたらおかしいのでその場合はFalseを返す
+            else:
+                break
+
+        sh.name = f"{sheet_name} ({counter})"
+        sh = wb.sheets.add(sheet_name, before=wb.sheets[position])
+
+    return sh
+
+
+def check_sheet_copy(sheet_source_name, wb_source=None, wb_destination=None, position=0):
+    """同名シートが存在するかチェックしてシートコピー
+
+    Args:
+        sheet_name (str): シート名
+        wb (xw.Book, optional): xw.Book. Defaults to None.
+        position (int, optional): 位置. Defaults to 0.
+
+    Returns:
+        bool: True:存在する, False:存在しない
+        sh: シート
+    """
+
+    if wb_destination == None:  # Excelが起動していない場合はFalseを返す
+        try:
+            wb_destination = xw.books.active
+        except:
+            return False
+
+    try:
+        sh = wb_source.sheets[sheet_source_name].copy(before=wb_destination.sheets[position])
+    except:
+        sh = wb_source.sheets[sheet_source_name]
+        all_sh_name = [sh.name for sh in wb_destination.sheets]
+        counter = 2
+
+        while True:
+            if f"{sheet_source_name} ({counter})" in all_sh_name:
+                counter += 1
+                if counter > 50:
+                    return False  # 50も作成してたらおかしいのでその場合はFalseを返す
+            else:
+                break
+
+        sh.name = f"{sheet_source_name} ({counter})"
+        sh = wb_source.sheets[sheet_source_name].copy(before=wb_destination.sheets[position])
+
+    return sh
